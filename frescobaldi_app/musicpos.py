9d62ea8aa8e1ee4d75c0580b9ae07820a74d7133
Benjamin Johnson <42893476+bmjcode@users.noreply.github.com>
2024-12-15 16:54:00 -0500

    Don't access the View across threads

--- frescobaldi_app/musicpos.py
+++ frescobaldi_app/musicpos.py
@@ -37,10 +37,8 @@ class MusicPosition(plugin.ViewSpacePlugin):
     def __init__(self, space):
         self._worker = MusicPositionWorker.create(self)
         self._worker.textChanged.connect(self._slotDisplayText)
-        self._timer = QTimer(singleShot=True,
-                             timeout=self._worker.slotTimeout)
-        self._waittimer = QTimer(singleShot=True,
-                                 timeout=self._worker.slotTimeout)
+        self._timer = QTimer(singleShot=True, timeout=self.slotTimeout)
+        self._waittimer = QTimer(singleShot=True, timeout=self.slotTimeout)
         self._label = QLabel()
         space.status.layout().insertWidget(1, self._label)
         self._view = lambda: None
@@ -75,6 +73,13 @@ class MusicPosition(plugin.ViewSpacePlugin):
         if not self._waittimer.isActive():
             self._timer.start(100)
 
+    def slotTimeout(self):
+        """Called when one of the timers fires."""
+        view = self._view()
+        if view:
+            self._worker.prepare(view.document(), view.textCursor())
+            QTimer.singleShot(0, self._worker.slotTimeout)
+
     def _slotDisplayText(self, text):
         self._label.setText(text)
         self._label.setVisible(bool(text))
@@ -88,13 +93,21 @@ class MusicPositionWorker(worker.Worker):
     thus reduces lag when editing larger files.
 
     """
+    def prepare(self, document, textCursor):
+        """Set the document and text cursor atomically."""
+        try:
+            self.mutex().lock()
+            self._document = document
+            self._textCursor = textCursor
+        finally:
+            self.mutex().unlock()
+
     def slotTimeout(self):
         """Called when one of the timers in the main thread fires."""
-        plugin = self.controller()
-        view = plugin._view()
-        if view:
-            d = view.document()
-            c = view.textCursor()
+        try:
+            self.mutex().lock()
+            d = self._document
+            c = self._textCursor
             import documentinfo
             m = documentinfo.music(d)
             import ly.duration
@@ -108,6 +121,10 @@ class MusicPositionWorker(worker.Worker):
                 text = _("Pos: {pos}").format(
                     pos=ly.duration.format_fraction(pos)) if pos is not None else ''
             self.textChanged.emit(text)
+        except AttributeError:
+            return  # no document or cursor available
+        finally:
+            self.mutex().unlock()
 
     # argument: text
     textChanged = pyqtSignal(str)
